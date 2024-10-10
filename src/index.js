4d94e4d545b7e6a074c67d4e3db14b60987d52ee
ducngo <duc.ngo@salto.vn>
2024-10-10 11:05:09 +0700

    feat: add mutation upload on graphql

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { WebSocketLink } from "@apollo/client/link/ws";
 import { SubscriptionClient } from "subscriptions-transport-ws";
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
+import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql'
@@ -28,6 +29,16 @@ const wsLink = new WebSocketLink(
   })
 );
 
+const token = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJuYW1lIjoidGluQGdtYWlsLmNvbSIsInBpY3R1cmUiOiIiLCJlbWFpbCI6InRpbkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImF1dGhfdGltZSI6MTcyODUzMTc4NSwidXNlcl9pZCI6IndONDlBNUZPcWNVS0NJQ0x1ekhnNk5tWTJBekgiLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInRpbkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9LCJpYXQiOjE3Mjg1MzE3ODUsImV4cCI6MTcyODUzNTM4NSwiYXVkIjoic29xdXJhLWJldGEtZGV2ZWxvcG1lbnQiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc29xdXJhLWJldGEtZGV2ZWxvcG1lbnQiLCJzdWIiOiJ3TjQ5QTVGT3FjVUtDSUNMdXpIZzZObVkyQXpIIn0.";
+
+const uploadLink = createUploadLink({
+  uri: 'http://localhost:4000/graphql',
+  headers: {
+    "Authorization": `Bearer ${token}`,
+    "Content-Type": "multipart"
+  }
+});
+
 // The split function takes three parameters:
 //
 // * A function that's called for each operation to execute
@@ -41,12 +52,13 @@ const splitLink = split(
       definition.operation === 'subscription'
     );
   },
-  wsLink,
-  httpLink,
+  // wsLink,
+  // httpLink,
+  uploadLink,
 );
 const client = new ApolloClient({
   // uri: 'http://localhost:3005/graphql',
-  link: splitLink,
+  link: uploadLink,
   cache: new InMemoryCache(),
 });
 
--- src/upload-file.js
+++ src/upload-file.js
@@ -1,6 +1,35 @@
+import { gql, useQuery, useMutation  } from '@apollo/client';
+const UPLOAD_FILE = gql`
+    mutation uploadFile($file: Upload!) {
+        uploadFile(audioFile: $file, duration: 10)
+    }
+` 
+
 function UploadFile() {
+    const [uploadFile] = useMutation(UPLOAD_FILE);
     return <>
     UploadFile <br />
+    <input
+      type="file"
+      required
+      onChange={({
+        target: {
+          validity,
+          files: [file],
+        },
+      }) => {
+        const newFile = new File([file],"ﾃｭﾃｬｵ9/10.mp3",  { type: file.type }) 
+        if (validity.valid)
+        //     file.name = "dustin.mp3"
+        // console.log(file.name);
+        
+            uploadFile({
+            variables: {
+              file: newFile,
+            },
+          });
+      }}
+    />
     </>
 }
 
